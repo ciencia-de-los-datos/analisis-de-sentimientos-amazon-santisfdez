64c7a309900248be9996e78dc3f6e7589b8df5bc
santisfdez <sasanchezfe@unal.edu.co>
2022-05-12 21:56:21 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -59,8 +59,8 @@ def pregunta_02():
     # aleatorios es 12345. Use el 10% de patrones para la muestra de prueba.
     x_train, x_test, y_train, y_test = train_test_split(
         x_tagged,
-        y_untagged,
-        test_size=0.10,
+        y_tagged,
+        test_size=0.1,
         random_state=12345,
     )
 
