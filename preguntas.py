8304679dc43cff5bcac9f014a5da6ae3562358e4
santisfdez <sasanchezfe@unal.edu.co>
2022-05-13 22:57:55 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -131,7 +131,7 @@ def pregunta_04():
     pipeline = Pipeline(
         steps=[
             ("vector", countVectorizer),
-            ("ber", BernoulliNB()),
+            ("bernoulli", BernoulliNB()),
         ],
     )
 
@@ -139,7 +139,7 @@ def pregunta_04():
     # considerar 10 valores entre 0.1 y 1.0 para el parámetro alpha de
     # BernoulliNB.
     param_grid = {
-        "ber_alpha": np.arange(0.1, 1.01, 1),
+        "bernoulli__alpha": np.arange(0.1, 1.01, 1),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
